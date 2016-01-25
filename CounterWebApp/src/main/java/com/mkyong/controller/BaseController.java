196916aa53f6efb73cb237ee86996fd7782bb53a
s.karthik <s.karthik@polarisft.com>
2016-01-25 10:02:28 +0530

    commit -m "CD-6 codes are modified and ready for build"

--- CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
+++ CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
@@ -33,7 +33,7 @@ public class BaseController {
 
 		model.addAttribute("message", "Welcome Polarite " + name);
 		model.addAttribute("message", "Welcome SCM PRACTICE TEAM " + name);
-		model.addAttribute("message", "Warm Welcome KARTHIKS" ) ;
+		model.addAttribute("message", "Warm Welcome HSBC Team " ) ;
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcomeName] counter : {}", counter);
 		return VIEW_INDEX;
